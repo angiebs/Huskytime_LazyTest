86c1eafe641e202c8c3fc6a3185080d0ba5bf357
Luis Toubes <luis@toub.es>
2015-10-21 00:51:39 +0200

    Added Dummy module and controller

---
+++ app/scripts/dummy/controllers/dummyCtrl.js
@@ -0,0 +1,20 @@
+define([
+    'angular',
+    'jquery',
+    'dummy/module'
+  ],
+  function (angular,$, ModuleManager) {
+    'use strict';
+
+    SearchModule.controller('DummyController',
+      [
+        '$scope',
+        '$log',
+        '$timeout',
+        function ($scope,$log, $timeout) {
+          //Variable Initialization
+
+        }
+      ]
+    );
+  });
---
+++ app/scripts/dummy/module.js
@@ -0,0 +1,19 @@
+define([
+    'angular',
+    'jquery',
+    'angular-ui-router'
+  ],
+  function (angular,$) {
+    'use strict';
+
+    /**
+     * @ngdoc Module Dummy
+     * @name huskytime
+     * @description
+     * # Features related to the Dummy Feature
+     *
+     */
+    var module = angular.module('huskytime.dummy', ['ui.router']);
+
+    return module;
+  });
